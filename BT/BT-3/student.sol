407c8037f1849a03ddba620cf8e516a48abd2eee
Shivtej Bhilare <bhilareshivtej@gmail.com>
2024-11-08 06:35:27 +0530

    BT

--- BT/BT-3/student.sol
+++ BT/BT-3/student.sol
@@ -1,48 +1,49 @@
-//SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Build the Contract
-contract StudentData
-{
-	// Create a structure for
-	// student details
-	struct Student
-	{
-		int ID;
-		string fName;
-		string lName;
-		int[2] marks;
-	}
-	// fallback () external payable {
-		
-	//  }
+contract StudentData {
+    // Structure to hold student details
+    struct Student {
+        uint id;
+        string name;
+        uint age;
+        string grade;
+    }
 
-	address owner;
-	int public stdCount = 0;
-	mapping(int => Student) public stdRecords;
+    // Array to store students
+    Student[] public students;
+    uint public studentCount = 0;
 
-	modifier onlyOwner
-	{
-		require(owner == msg.sender);
-		_;
-	}
-	constructor()
-	{
-		owner=msg.sender;
-	}
+    // Function to add a new student
+    function addStudent(string memory _name, uint _age, string memory _grade) public {
+        studentCount++;
+        students.push(Student(studentCount, _name, _age, _grade));
+    }
 
-	// Create a function to add
-	// the new records
-	function addNewRecords(int _ID,string memory _fName,string memory _lName,int[2] memory _marks) public onlyOwner
-	{
-		// Increase the count by 1
-		stdCount = stdCount + 1;
+    // Function to get a student's details by id
+    function getStudent(uint _id) public view returns (string memory, uint, string memory) {
+        for (uint i = 0; i < students.length; i++) {
+            if (students[i].id == _id) {
+                return (students[i].name, students[i].age, students[i].grade);
+            }
+        }
+        revert("Student not found");
+    }
 
-		// Fetch the student details
-		// with the help of stdCount
-		stdRecords[stdCount] = Student(_ID, _fName,_lName, _marks);
-	}
+    // Fallback function to handle ether sent directly to the contract
+    fallback() external payable {
+        // This will just accept ether sent to the contract
+    }
 
+    // Receive function to handle plain ether transfers
+    receive() external payable {
+        // This will accept ether sent to the contract
+    }
+
+    // Function to check the balance of the contract
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
 
 
